43d77707d4e7e075b7ad55423f4864d8dbf4dd40
Patrick Edson <pedson@users.noreply.github.com>
2018-10-11 11:56:20 -0400

    Differents body contents for graphql-upload on server side.

--- src/UploadNetworkInterface.js
+++ src/UploadNetworkInterface.js
@@ -1,77 +1,94 @@
-import { printAST } from 'apollo-client'
-import { HTTPFetchNetworkInterface, printRequest } from 'apollo-client/transport/networkInterface'
+import {printAST} from 'apollo-client'
+import {HTTPFetchNetworkInterface, printRequest} from 'apollo-client/transport/networkInterface'
 
 export default function createNetworkInterface(opts) {
-  const { uri } = opts
-  return new UploadNetworkInterface(uri, opts)
+    const {uri} = opts
+    return new UploadNetworkInterface(uri, opts)
 }
 
 export class UploadNetworkInterface extends HTTPFetchNetworkInterface {
 
-  fetchFromRemoteEndpoint(req) {
-    const options = this.isUpload(req)
-      ? this.getUploadOptions(req)
-      : this.getJSONOptions(req)
-    return fetch(this._uri, options);
-  }
-
-  isUpload({ request }) {
-    if (request.variables) {
-      for (let key in request.variables) {
-        if (request.variables[key] instanceof FileList) {
-          return true
-        }
-        if ((request.variables[key] instanceof Array) && (request.variables[key].length > 0)) {
-          return request.variables[key][0] instanceof File
-        }
-      }
-    }
-    return false
-  }
-
-  getJSONOptions({ request, options }) {
-    return Object.assign({}, this._opts, {
-      body: JSON.stringify(printRequest(request)),
-      method: 'POST',
-    }, options, {
-      headers: Object.assign({}, {
-        Accept: '*/*',
-        'Content-Type': 'application/json',
-      }, options.headers),
-    })
-  }
-
-  getUploadOptions({ request, options }) {
-    const body = new FormData()
-    const variables = {}
-
-    for (let key in request.variables) {
-      let v = request.variables[key]
-      let list =  null;
-      if (v instanceof FileList) {
-          list = Array.from(v);
-      } else if (v instanceof Array && (v.length > 0) && v[0] instanceof File) {
-          list = v;
-      }
-      if (list) {
-          list.forEach(f => body.append(key, f))
-      } else {
-        variables[key] = v
-      }
+    fetchFromRemoteEndpoint(req) {
+        const options = this.isUpload(req)
+            ? this.getUploadOptions(req)
+            : this.getJSONOptions(req)
+        return fetch(this._uri, options);
     }
 
-    body.append('operationName', request.operationName)
-    body.append('query', printAST(request.query))
-    body.append('variables', JSON.stringify(variables))
+    isUpload({request}) {
+        if (request.variables) {
+            for (let key in request.variables) {
+                if (request.variables[key] instanceof FileList) {
+                    return true
+                }
+                if ((request.variables[key] instanceof Array) && (request.variables[key].length > 0)) {
+                    return request.variables[key][0] instanceof File
+                }
+            }
+        }
+        return false
+    }
 
-    return Object.assign({}, this._opts, {
-      body,
-      method: 'POST',
-    }, options, {
-      headers: Object.assign({}, {
-        Accept: '*/*',
-      }, options.headers),
-    })
-  }
+    getJSONOptions({request, options}) {
+        return Object.assign({}, this._opts, {
+            body: JSON.stringify(printRequest(request)),
+            method: 'POST',
+        }, options, {
+            headers: Object.assign({}, {
+                Accept: '*/*',
+                'Content-Type': 'application/json',
+            }, options.headers),
+        })
+    }
+
+    getUploadOptions({request, options}) {
+        const body = new FormData();
+        const variables = {};
+        let fileCount = 0;
+        const map = {};
+        const file = [];
+
+        for (let key in request.variables) {
+            let v = request.variables[key]
+            let list = null;
+            if (v instanceof FileList) {
+                list = Array.from(v);
+            } else if (v instanceof Array && (v.length > 0) && v[0] instanceof File) {
+                list = v;
+            }
+            if (list) {
+                list.forEach((f, idx) => {
+                    // body.append(key, f)
+                    variables[key] = null;
+                    map[fileCount++] = [`variables.${key}.${idx}`];
+                    file.push(f);
+                });
+
+            } else {
+                variables[key] = v
+            }
+        }
+
+        // body.append('operationName', request.operationName)
+        // body.append('query', printAST(request.query))
+        // body.append('variables', JSON.stringify(variables))
+
+        body.append('operations', JSON.stringify({query: printAST(request.query), variables}));
+
+        if (fileCount > 0) {
+            body.append('map', JSON.stringify(map));
+            file.forEach((f, idx) => body.append(`${idx}`, f));
+        }
+
+
+        return Object.assign({}, this._opts, {
+            body,
+            method: 'POST',
+        }, options, {
+            headers: Object.assign({}, {
+                Accept: '*/*',
+            }, options.headers),
+        })
+    }
 
 }
